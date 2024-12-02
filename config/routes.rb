5b720aea5cd3d6b7095ab6f2543936575d9999bf
Tomislav Celic <celer.tomislav@gmail.com>
2024-12-02 16:13:04 +0100

    Translate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,23 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  devise_for :users
-  get 'profile/:id', to: 'users#show', as: 'user_profile'
-  get 'profile/:id/connections', to: 'users#connections', as: 'user_connections'
+  devise_for :users, path: 'korisnici', path_names: {
+  sign_in: 'prijava',
+  sign_out: 'odjava',
+  sign_up: 'registracija',
+  password: 'lozinka',
+  confirmation: 'potvrda',
+  unlock: 'otkljucaj',
+  edit: 'uredi'
+}
+
+  get 'profil/:id', to: 'users#show', as: 'user_profile'
+  get 'profil/:id/veze', to: 'users#connections', as: 'user_connections'
 
   resources :users, only: [:show] do
     member do
-      post 'follow'
-      delete 'unfollow'
+      post 'prati'
+      delete 'odprati'
     end
   end  
 end
