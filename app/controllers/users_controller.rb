db28fbd60de4b395b55edee26b2adaed0433cdd1
Tomislav Celic <celer.tomislav@gmail.com>
2024-12-02 16:08:36 +0100

    Fix Users Controller - user could follow himself & size and count method missmatch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @is_current_user = @user == current_user
     @followers_count = @user.followers.count
-    @following_count = @user.followees.size
+    @following_count = @user.followees.count
   end
 
   def follow
     @user = User.find(params[:id])
-    current_user.following_relationships.create(followee: @user) unless current_user.following?(@user)
+    
+    if @user == current_user
+      flash[:alert] = t('views.users.show.cannot_follow_yourself')
+    elsif !current_user.following?(@user)
+      current_user.following_relationships.create(followee: @user)
+      flash[:notice] = t('views.users.show.followed_successfully')
+    else
+      flash[:alert] = t('views.users.show.already_following')
+    end
+  
     redirect_to user_profile_path(@user)
-  end
+  end  
 
   def unfollow
     @user = User.find(params[:id])
